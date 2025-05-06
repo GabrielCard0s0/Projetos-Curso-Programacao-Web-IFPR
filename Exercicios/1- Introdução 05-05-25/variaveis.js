94c53fa53de3814469569e4629605e06ca3253e3
Gabriel Cardoso <gabrielcardoso.s1998@gmail.com>
2025-05-05 21:54:49 -0300

    Create variaveis.js

---
+++ Exercicios/1- Introdução 05-05-25/variaveis.js
@@ -0,0 +1,54 @@
+// Exercícios
+
+// 1. Qual é a saída da função console.log() nas linhas 2, 3, 5 e 6?
+
+let flag = true
+console.log("Valor:", flag)
+console.log("Tipo:", typeof flag)
+flag = "Hello, World!"
+console.log("Valor:", flag)
+console.log("Tipo:", typeof flag)
+
+// 2. Classifique os nomes das variáveis como válidos ou inválidos.
+// (I) 1num invalido
+// (II) _num valido
+// (III) num_ valido
+// (IV) def valido
+// (V) nu-m invalido
+
+// 3. Qual é o valor e o tipo da variável Num na linha 3?
+
+// let Numero = 4.5;
+// let numero = 4;
+console.log(Numero);
+console.log(typeof Numero)
+
+// 4. O que significa dizer que a linguagem JavaScript é case-sensitive?
+// 5. O que são palavras reservadas em uma linguagem de programação?
+
+// Exercícios
+
+// 6. Declare variáveis para armazenar as seguintes informações sobre um filme:
+
+// O título do filme.
+// O ano de lançamento.
+// A duração em minutos.
+// Se é classificado como adequado para maiores de 18 anos.
+
+// 7. Crie variáveis para armazenar as seguintes informações de um funcionário:
+
+// Nome completo.
+// Cargo.
+// Tempo de empresa (em anos).
+// Se o funcionário está ativo.
+
+// Exercícios
+
+// 8. Dada uma variável idade com um número inteiro, crie uma variável idade_str que
+// converta esse número para uma string. Em seguida, crie uma frase que diga "Eu
+// tenho [idade] anos."
+
+
+// 9. Dado o nome, sobrenome e idade de uma pessoa, usando concatenação de string,
+// combine essas variáveis em uma única frase, que diga "Meu nome é [nome
+// completo] e eu tenho [idade] anos."
