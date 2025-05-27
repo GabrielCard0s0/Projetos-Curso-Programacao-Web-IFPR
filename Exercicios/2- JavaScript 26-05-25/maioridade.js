8f801450d98f31f981e5f784df1c7d8c0a3745ed
Gabriel Cardoso <gabrielcardoso.s1998@gmail.com>
2025-05-26 21:44:05 -0300

    novo conteudo

---
+++ Exercicios/2- JavaScript 26-05-25/maioridade.js
@@ -0,0 +1,9 @@
+let idade = Number(prompt(`Digite sua idade:`))
+
+if(idade >= 18){
+    console.log(`Você é maior de idade.`)
+    alert(`Você é de maior.`)
+}else{
+    console.log(`Você é menor de idade.`)
+    alert(`Você é de menor.`)
+}
